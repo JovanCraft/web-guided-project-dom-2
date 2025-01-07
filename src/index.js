b765d30a5c770dbaaa7a1bcd5305aec192d402b8
jovan <jovancraft@gmail.com>
2025-01-07 17:43:54 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -65,3 +65,5 @@ function killReports() {
 // 👉 TASK 9- [STRETCH] Using the mousemove event on 'document',
 // log to the console the X and Y coordinates
 // of the mouse pointer, as it moves over the screen.
+
+
